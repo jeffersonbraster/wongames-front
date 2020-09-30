fbda8a159535956e7ba2240fa0908332dda8b0dc
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-29 22:20:39 -0300

    Melhorando acessibilidade e realizando testes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,19 +7,20 @@ import * as S from './styles'
 const Menu = () => (
   <S.Wrapper>
     <S.IconWrapper>
-      <MenuIcon />
+      <MenuIcon aria-label="Open menu" />
     </S.IconWrapper>
 
     <S.LogoWrapper>
       <Logo hideOnMobile />
     </S.LogoWrapper>
+
     <S.MenuGroup>
       <S.IconWrapper>
-        <SearchIcon />
+        <SearchIcon aria-label="search" />
       </S.IconWrapper>
 
       <S.IconWrapper>
-        <ShoppingCartIcon />
+        <ShoppingCartIcon aria-label="Open shopping cart" />
       </S.IconWrapper>
     </S.MenuGroup>
   </S.Wrapper>
