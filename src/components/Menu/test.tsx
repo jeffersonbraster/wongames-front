d77fdf04a9b593a28e65047083ec1655ef99cb30
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-29 22:42:50 -0300

    Criando logica de abrir/fechar com react hooks e TDD

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Menu from '.'
 
@@ -10,5 +10,26 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should render the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    //selecionar menufull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    //verificar se esta escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    //clicar no botao abrir menu e ver se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    //clicar no botao de fechar e verificar se fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
