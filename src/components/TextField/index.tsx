80e933e34af87ce62fc0a3ea9d37cca718f07d30
Jefferson <jeffersonbraster@gmail.com>
2020-10-20 10:51:13 -0300

    Colocando a opção de disabled no textfield

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -8,6 +8,7 @@ export type TextFieldProps = {
   initialValue?: string
   icon?: React.ReactNode
   iconPosition?: 'left' | 'right'
+  disabled?: boolean
 } & InputHTMLAttributes<HTMLInputElement>
 
 const TextField = ({
@@ -17,6 +18,7 @@ const TextField = ({
   label,
   labelFor = '',
   initialValue = '',
+  disabled = false,
   ...props
 }: TextFieldProps) => {
   const [value, setValue] = useState(initialValue)
@@ -29,7 +31,7 @@ const TextField = ({
   }
 
   return (
-    <S.Wrapper>
+    <S.Wrapper disabled={disabled}>
       {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
@@ -38,6 +40,7 @@ const TextField = ({
           type="text"
           onChange={onChange}
           value={value}
+          disabled={disabled}
           {...props}
         />
       </S.InputWrapper>
