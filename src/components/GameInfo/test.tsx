ece2e13c176923fd7e294978d3f8563bf8d58cba
Jefferson <jeffersonbraster@gmail.com>
2020-11-14 10:56:15 -0300

    Criando estrutura do GameInfo, fazendo pelo TDD

---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import GameInfo from '.'
+
+const props = {
+  title: 'My Game Title',
+  description: 'Game Description',
+  price: '210,00'
+}
+
+describe('<GameInfo />', () => {
+  it('should render game information', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    //esperar um heading (title)
+    expect(
+      screen.getByRole('heading', { name: /my game title/i })
+    ).toBeInTheDocument()
+
+    //esperar pelo description
+    expect(screen.getByText(/\$210,00/i)).toBeInTheDocument()
+
+    //esperar pelo price
+    expect(screen.getByText(/game description/i)).toBeInTheDocument()
+  })
+
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    //esperar um button add to cart
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+
+    //esperar por button wishlist
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+  })
+})
