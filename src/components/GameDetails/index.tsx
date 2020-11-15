c1aa17db5afd1be60518e85d2625e22962fb3f61
Jefferson <jeffersonbraster@gmail.com>
2020-11-14 22:35:59 -0300

    Adicionando icones na plataforma

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -1,46 +1,66 @@
 import Heading from 'components/Heading'
 import MediaMatch from 'components/MediaMatch'
+import { Apple, Windows, Linux } from '@styled-icons/fa-brands'
 import * as S from './styles'
 
-const GameDetails = () => (
-  <S.Wrapper>
-    <MediaMatch greaterThan="small">
-      <Heading lineLeft lineColor="secondary">
-        Game Details
-      </Heading>
-    </MediaMatch>
+type Platforms = 'windows' | 'linux' | 'mac'
 
-    <S.Content>
-      <S.Block>
-        <S.Title>Developer</S.Title>
-        <S.Description>Gearbox Software</S.Description>
-      </S.Block>
+export type GameDetailsProps = {
+  platforms: Platforms[]
+}
 
-      <S.Block>
-        <S.Title>Release Date</S.Title>
-        <S.Description>Nov 16, 2019</S.Description>
-      </S.Block>
+const GameDetails = ({ platforms }: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux title="Linux" size={18} />,
+    mac: <Apple title="mac" size={18} />,
+    windows: <Windows title="mac" size={18} />
+  }
 
-      <S.Block>
-        <S.Title>Platforms</S.Title>
-      </S.Block>
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
 
-      <S.Block>
-        <S.Title>Publisher</S.Title>
-        <S.Description>2k</S.Description>
-      </S.Block>
+      <S.Content>
+        <S.Block>
+          <S.Title>Developer</S.Title>
+          <S.Description>Gearbox Software</S.Description>
+        </S.Block>
 
-      <S.Block>
-        <S.Title>Rating</S.Title>
-        <S.Description>18+</S.Description>
-      </S.Block>
+        <S.Block>
+          <S.Title>Release Date</S.Title>
+          <S.Description>Nov 16, 2019</S.Description>
+        </S.Block>
 
-      <S.Block>
-        <S.Title>Genres</S.Title>
-        <S.Description>Action / Adventure</S.Description>
-      </S.Block>
-    </S.Content>
-  </S.Wrapper>
-)
+        <S.Block>
+          <S.Title>Platforms</S.Title>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platforms) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Publisher</S.Title>
+          <S.Description>2k</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Rating</S.Title>
+          <S.Description>18+</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Genres</S.Title>
+          <S.Description>Action / Adventure</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
 
 export default GameDetails
--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,16 +1,25 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
 export default {
   title: 'Game/GameDetails',
   component: GameDetails,
   parameters: {
     backgrounds: { default: 'won-dark' }
+  },
+  args: { platforms: ['windows', 'linux', 'mac'] },
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    }
   }
 } as Meta
 
-export const Basic: Story = (args) => (
+export const Basic: Story<GameDetailsProps> = (args) => (
   <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-    <GameDetails />
+    <GameDetails {...args} />
   </div>
 )
