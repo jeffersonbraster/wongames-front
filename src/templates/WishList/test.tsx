52e3190110a08e92897d9003380f28cf5e34ee32
Jefferson <jeffersonbraster@gmail.com>
2020-11-21 23:51:41 -0300

    Realizando os primeiros testes

--- src/templates/WishList/test.tsx
+++ src/templates/WishList/test.tsx
@@ -1,13 +1,29 @@
-import { render, screen } from '@testing-library/react'
-import WishList from '.'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+import Wishlist from '.'
+
+const props = {
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
 
 describe('<WishList />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<WishList />)
+  it('should render correctly', () => {
+    renderWithTheme(<Wishlist {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /WishList/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+      screen.getByRole('heading', { name: /wishlist/i })
+    ).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
   })
 })
