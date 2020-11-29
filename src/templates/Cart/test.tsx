0e705b4aaa6a2aa476ca53d8e38155be005adc5b
Jefferson <jeffersonbraster@gmail.com>
2020-11-29 01:18:42 -0300

    Criando a pagina do cart

---
+++ src/templates/Cart/test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import Cart from '.'
+
+describe('<Cart />', () => {
+  it('should render the heading', () => {
+    render(<Cart />)
+  })
+})
