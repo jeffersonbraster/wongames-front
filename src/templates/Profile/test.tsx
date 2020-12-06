132e8a0602d40797b0db45acb7ab25c51011b83f
Jefferson <jeffersonbraster@gmail.com>
2020-12-06 00:55:35 -0300

    Criando o OrdersList completo

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -1,13 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import Profile from '.'
 
 describe('<Profile />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Profile />)
-
-    expect(
-      screen.getByRole('heading', { name: /Profile/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    renderWithTheme(<Profile />)
   })
 })
