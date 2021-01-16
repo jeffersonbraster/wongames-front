57b70a33642f23c2a72ba80142f503835932de30
Jefferson <jeffersonbraster@gmail.com>
2021-01-16 14:48:47 -0300

    criando estilos

--- src/components/ExploreSidebar/stories.tsx
+++ src/components/ExploreSidebar/stories.tsx
@@ -17,12 +17,17 @@ export default {
 } as Meta
 
 export const Basic: Story<ExploreSidebarProps> = (args) => (
-  <ExploreSidebar {...args} />
+  <div style={{ padding: 16, maxWidth: 320 }}>
+    <ExploreSidebar {...args} />
+  </div>
 )
 
 export const WithInitialValues: Story<ExploreSidebarProps> = (args) => (
-  <ExploreSidebar
-    {...args}
-    initialValues={{ windows: true, sort_by: 'low-to-high' }}
-  />
+  <div style={{ padding: 16, maxWidth: 320 }}>
+    {' '}
+    <ExploreSidebar
+      {...args}
+      initialValues={{ windows: true, sort_by: 'low-to-high' }}
+    />
+  </div>
 )
