3ca864c77f6dfa569283c936a27f1dcfb72294c8
Jefferson <jeffersonbraster@gmail.com>
2021-01-15 22:16:17 -0300

    exploresidebar criando e fazendo primeiras estruturas

---
+++ src/components/ExploreSidebar/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ExploreSidebar, { ExploreSidebarProps } from '.'
+
+import items from './mock'
+
+export default {
+  title: 'ExploreSidebar',
+  component: ExploreSidebar,
+  args: {
+    items
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Basic: Story<ExploreSidebarProps> = (args) => (
+  <ExploreSidebar {...args} />
+)
