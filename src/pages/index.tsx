611a93e7d4fe5584571c187212348bd3f8480b38
Jefferson <jeffersonbraster@gmail.com>
2021-01-24 22:43:05 -0300

    Populando a página de games com dados via getStaticProps com revalidade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,13 @@
-import { gql, useQuery } from '@apollo/client'
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
-  const { data, loading, error } = useQuery(gql`
-    query getGames {
-      games {
-        name
-      }
-    }
-  `)
-
-  if (loading) return <p>Carregando...</p>
-  if (error) return <p>{error}</p>
-  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
-
   return <Home {...props} />
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
