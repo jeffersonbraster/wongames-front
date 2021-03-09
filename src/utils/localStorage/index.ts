43607b52b1edad6771d22f5d34c390c6cf72a253
Jefferson <jeffersonbraster@gmail.com>
2021-03-08 22:55:38 -0300

    Criando um utilitario para trabalhar com o localstorage

---
+++ src/utils/localStorage/index.ts
@@ -0,0 +1,18 @@
+const APP_KEY = 'WONGAMES'
+
+export function getStorageItem(key: string) {
+  // no next via server/static não tem window, precisa verificar para nao quebrar.
+  if (typeof window === 'undefined') return
+
+  const data = window.localStorage.getItem(`${APP_KEY}_ ${key}`)
+
+  return JSON.parse(data!)
+}
+
+export function setStorageItem(key: string, value: string[]) {
+  if (typeof window === 'undefined') return
+
+  const data = JSON.stringify(value)
+
+  return window.localStorage.setItem(`${APP_KEY}_${key}`, data)
+}
