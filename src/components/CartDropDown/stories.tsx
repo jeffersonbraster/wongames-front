caca11f74cc8073ff82c64efedd4bb319e4be414
Jefferson <jeffersonbraster@gmail.com>
2021-03-11 19:45:33 -0300

    Editando CartDropDown para adaptar ao context

--- src/components/CartDropDown/stories.tsx
+++ src/components/CartDropDown/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CartDropDown, { CartDropDownProps } from '.'
+import CartDropDown from '.'
 import items from 'components/CartList/mock'
 
 export default {
@@ -9,20 +9,24 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
-  },
-  args: {
-    items,
-    total: 'R$ 300,00'
   }
 } as Meta
 
-export const Basic: Story<CartDropDownProps> = (args) => (
+export const Basic: Story = (args) => (
   <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
     <CartDropDown {...args} />
   </div>
 )
 
-export const Empty: Story<CartDropDownProps> = () => (
+Basic.args = {
+  cartContextValue: {
+    items,
+    quantity: items.length,
+    total: 'R$ 300,00'
+  }
+}
+
+export const Empty: Story = () => (
   <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
     <CartDropDown />
   </div>
--- src/components/CartDropDown/test.tsx
+++ src/components/CartDropDown/test.tsx
@@ -1,20 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import { render, screen } from 'utils/test-utils'
 import CartDropDown from '.'
 
 import items from 'components/CartList/mock'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartDropDown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    renderWithTheme(<CartDropDown items={items} total="R$ 300" />)
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 300'
+    }
 
+    render(<CartDropDown />, { cartProviderProps })
+  })
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render dropdown content with cart items and total', () => {
-    renderWithTheme(<CartDropDown items={items} total="R$ 300" />)
-
     expect(screen.getByText('R$ 300')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
