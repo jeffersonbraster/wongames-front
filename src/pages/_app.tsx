985e475889318b8d6e73a0c85d4c30f5eec2da62
Jefferson <jeffersonbraster@gmail.com>
2021-03-13 17:56:07 -0300

    Add AuthProvider do nextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import NextNprogrss from 'nextjs-progressbar'
+import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { CartProvider } from 'hooks/use-cart'
 import type { AppProps } from 'next/app'
@@ -13,30 +14,32 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="The best Games Stores in the word"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="The best Games Stores in the word"
+              />
+            </Head>
+            <GlobalStyles />
+            <NextNprogrss
+              color="#f231a5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
             />
-          </Head>
-          <GlobalStyles />
-          <NextNprogrss
-            color="#f231a5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
