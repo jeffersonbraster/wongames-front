12bb70bd8492399c132461fec6407d7dd4054cdb
Jefferson <jeffersonbraster@gmail.com>
2021-03-30 21:18:04 -0300

    Editando a pagina de profile/me

--- src/components/FormForgotPassword/index.tsx
+++ src/components/FormForgotPassword/index.tsx
@@ -13,12 +13,15 @@ import {
 } from 'components/Form'
 import Button from '../Button'
 import { FieldErrors, forgotValidate } from 'utils/validations'
+import { useRouter } from 'next/router'
 
 const FormForgotPassword = () => {
+  const { query } = useRouter()
+
   const [success, setSuccess] = useState(false)
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ email: '' })
+  const [values, setValues] = useState({ email: (query.email as string) || '' })
   const [loading, setLoading] = useState(false)
 
   const handleInput = (field: string, value: string) => {
@@ -80,6 +83,7 @@ const FormForgotPassword = () => {
               name="email"
               placeholder="E-mail"
               error={fieldError?.email}
+              initialValue={query.email as string}
               onInputChange={(v) => handleInput('email', v)}
               type="email"
               icon={<Email />}
