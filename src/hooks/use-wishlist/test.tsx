e3530d4c722c5214c9f1f85ca95f5689f2351d85
Jefferson <jeffersonbraster@gmail.com>
2021-04-11 00:18:58 -0300

    Criando mocks e estrutura inicial de teste do useWishlist

---
+++ src/hooks/use-wishlist/test.tsx
@@ -0,0 +1,19 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { renderHook } from '@testing-library/react-hooks'
+import { useWishlist, WishlistProvider } from '.'
+import { wishlistMock } from './mock'
+
+describe('useWishlist', () => {
+  it('should return wishlist items', () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[wishlistMock]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result } = renderHook(() => useWishlist(), { wrapper })
+
+    //it starts loading the data
+    expect(result.current.loading).toBe(true)
+  })
+})
